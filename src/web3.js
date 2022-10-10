45f4a0ef6cf7210c846b37dfd1b93e71dcb03890
Shubham Dubey <shubham@Shubhams-MacBook-Air.local>
2022-10-10 21:55:41 +0530

    creating new repo for frontend

---
+++ src/web3.js
@@ -0,0 +1,18 @@
+import Web3 from 'web3';
+
+const getWeb3 = async() => {
+    if(window.ethereum){
+        await window.ethereum.request({ method: 'eth_requestAccounts' });
+        console.log('MetaMask detected!');
+        return new Web3(window.ethereum);
+    }
+    else if (window.web3) {
+        console.log('inside window.web3')
+        return new Web3(window.web3.currentProvider);
+        // Acccounts always exposed
+    } else {
+        console.log('Non-Ethereum browser detected. You should consider trying MetaMask!');
+    }
+}
+
+export default getWeb3;
